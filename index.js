257c8aa47dd1410bd8af1e4281801c7e89baab50
amingilani <amin@gilani.me>
2016-12-30 03:50:43 +0500

    Add semantic ui to html, and helper npm scripts

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var static = require('serve-static');
 var server = connect();
 
 server.use(static(__dirname + "/src/views"));
+server.use(static(__dirname + "/bower_components/"));
 
 server.listen(3000);
 
 var livereload = require('livereload');
 var lrserver = livereload.createServer();
 lrserver.watch(__dirname + "/src/views");
+console.log('Server listening at localhost:3000');
